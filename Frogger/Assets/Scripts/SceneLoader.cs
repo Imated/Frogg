f5662ed325f7705ec646e304719617e8d877f7b1
EliTE08 <memeinsta84@gmail.com>
2025-12-17 03:24:31 -0500

    Added Quit Button

--- Frogger/Assets/Scripts/SceneLoader.cs
+++ Frogger/Assets/Scripts/SceneLoader.cs
@@ -1,3 +1,4 @@
+using System.Net.Mime;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,6 +7,11 @@ public class SceneLoader : MonoBehaviour
     public void LoadSpecificScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
 }
