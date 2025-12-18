74115a1e1a5bf7911c70a15b4ec513c9558c6e2a
EliTE08 <memeinsta84@gmail.com>
2025-12-18 01:37:17 -0500

    C4 Now Respawns

--- Frogger/Assets/Scripts/C4.cs
+++ Frogger/Assets/Scripts/C4.cs
@@ -5,21 +5,35 @@ using UnityEngine;
 public class C4 : MonoBehaviour
 {
     [SerializeField] private Vector2 force;
-    
+
+    private Collider2D col;
+    private SpriteRenderer sr;
+
+    private void Awake()
+    {
+        col = GetComponent<Collider2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             var rb = other.transform.parent.GetComponent<Rigidbody2D>();
             rb.AddForce(force * 10000);
+
             StartCoroutine(nameof(Respawn));
-            gameObject.SetActive(false);
+
+            col.enabled = false;
+            sr.enabled = false;
         }
     }
 
     private IEnumerator Respawn()
     {
         yield return new WaitForSeconds(3f);
-        gameObject.SetActive(true);
+
+        col.enabled = true;
+        sr.enabled = true;
     }
 }
